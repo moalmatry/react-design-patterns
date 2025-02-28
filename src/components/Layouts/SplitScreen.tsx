8918b571fba72f48a6577e1e481dfa8110930ef2
Moahmed Almarty <moalmatry2000@gmail.com>
2025-02-28 19:35:30 +0200

    feat: add nextjs

---
+++ src/components/Layouts/SplitScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+export interface SplitScreenProps {
+  left: React.ReactNode;
+  right: React.ReactNode;
+}
+
+const SplitScreen = () => {
+  return <div>SplitScreen</div>;
+};
+
+export default SplitScreen;
